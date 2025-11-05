bbe4be0b4519e7f6fdacab5a7b4cb71d46a04182
aokorodu <aokorodu@gmail.com>
2025-11-05 10:28:10 -0500

    minor styling tweaks

--- CSP-react/src/components/uicomponents/Header/Header.tsx
+++ CSP-react/src/components/uicomponents/Header/Header.tsx
@@ -1,12 +1,13 @@
 import styles from './Header.module.css';
 import BookIcon from "../../../assets/book.svg";
+import GraduationCapIcon from "../../../assets/graduationCap.svg";
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.titleHolder}>
                 <h1 className={styles.title}>529 College Savings Planner</h1>
-                <img src={BookIcon} alt="Book icon" className={styles.icon} width={40} height={40} />
+                <img src={GraduationCapIcon} alt="Book icon" className={styles.icon} width={50} height={50} />
             </div>
 
             <p className={styles.subtitle}>Figure out how much you need to save for college.</p>
