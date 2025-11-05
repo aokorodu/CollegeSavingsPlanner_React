56d534731bc0bb41de3e4e2027222504c67435d3
aokorodu <aokorodu@gmail.com>
2025-11-04 19:22:28 -0500

    responsive styling

--- CSP-react/src/components/PieChart/PieChart.tsx
+++ CSP-react/src/components/PieChart/PieChart.tsx
@@ -41,7 +41,7 @@ const PieChart = forwardRef((props, ref) => {
 
     return (
         <div className={styles.pieChartContainer}>
-            <svg id="svg" width="800" height="500" viewBox="0 0 1000 1000">
+            <svg id="svg" width="100%" height="100%" viewBox="0 0 1000 1000">
                 <defs>
                     <linearGradient
                         id="numberBackgroundGradient"
