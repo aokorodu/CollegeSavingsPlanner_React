a2aa57f0c5ed30ff407159c109bf45ddc001cdae
aokorodu <aokorodu@gmail.com>
2025-11-10 19:40:27 -0500

    tweaking desgin

--- CSP-react/src/components/uicomponents/Header/Header.tsx
+++ CSP-react/src/components/uicomponents/Header/Header.tsx
@@ -1,15 +1,15 @@
 import styles from './Header.module.css';
-import GraduationCapIcon from "../../../assets/graduationCap.svg";
+import GraduationCapIcon from "../../../assets/babyWithCap_min.svg";
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.titleHolder}>
                 <h1 className={styles.title}>529 College Savings Calculator</h1>
-                <img src={GraduationCapIcon} alt="graduation cap" className={styles.icon} width={50} height={50} />
+                <p className={styles.subtitle}>Calculate how much to set aside for your child's education.</p>
             </div>
 
-            <p className={styles.subtitle}>Calculate how much to set aside for your child's education.</p>
+            <img src={GraduationCapIcon} alt="graduation cap" className={styles.icon} width={"80px"} height={"80px"} />
         </header>
     );
 };
